b037a73a95f304e155828b2f3fe5303d1b38e951
Michael <michael@gmail.com>
2025-02-07 10:46:11 +0100

    changed the implementation to calculate the number of iterations required to solve the puzzle

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static int iterations = 0;
+
     public static void main(String[] args) {
-        towerOfHanoi(3, 'a', 'c', 'b');
+        System.out.print("Please enter the amount of disks: ");
+        String userInput = SCANNER.next();
+        while (!userInput.chars().allMatch(Character::isDigit)) {
+            System.out.print("Invalid input! Please enter a valid number [0-9]: ");
+            userInput = SCANNER.next();
+        }
+
+        towerOfHanoi(Integer.parseInt(userInput), 'a', 'c', 'b');
+        System.out.println("The method requires " + iterations + " iterations to solve the puzzle.");
+        SCANNER.close();
     }
 
     private static void towerOfHanoi(int n, char a, char b, char c) {
         if (n == 0) {
             return;
         }
+
+        iterations++;
         towerOfHanoi(n - 1, a, c, b);
-        System.out.println("Moving disk #" + n + " from rod " + a + " to rod " + b);
         towerOfHanoi(n - 1, c, b, a);
     }
 }
